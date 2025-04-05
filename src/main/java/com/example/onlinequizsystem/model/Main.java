a30f46ab022790858242d322db87e98add42146a
CongHuyKieuVHS <conghuykieu2005@gmail.com>
2025-04-05 14:17:11 -0400

    plssssssssss

--- src/main/java/com/example/onlinequizsystem/model/Main.java
+++ src/main/java/com/example/onlinequizsystem/model/Main.java
@@ -9,7 +9,7 @@ public class Main {
         List<Question> questions = new ArrayList<Question>();
         questions.add(new Question("is bao anh in adapted service?","yes"));
         questions.add(new Question("what is bao anh favorite color?","blue"));
-
+        //thats a quiz
         Quiz quiz = new Quiz(1,"general knowledge",Subject.COMPUTER_SCIENCE,questions);
 
         List<String> userAnswers = new ArrayList<>();
